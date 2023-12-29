5dae8ebc199395c1411aaa787738ec417120d4c3
victorbrax <joaogomespcl@gmail.com>
2023-12-29 14:26:10 -0300

    inserção da tabela de relação entre users e roles

--- app/admin/user_views.py
+++
@@ -1,7 +0,0 @@
-# from flask import current_app
-# from app import db
-# from app.models.users import User
-# from flask_admin import Admin
-
-# admin = current_app.extensions["Admin"]
-# admin.add_view(ModelView(User, db.session))
