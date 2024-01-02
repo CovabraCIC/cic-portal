1bd3b544b254fe03cb98c439dbbfad852f423beb
CovabraCIC <cic@covabra.com.br>
2024-01-02 11:07:56 -0300

    finalizacao dos flashs de registro

--- app/controllers/auth/accounts/forms.py
+++ app/controllers/auth/accounts/forms.py
@@ -10,6 +10,6 @@ class RegistrationForm(FlaskForm):
     first_name = StringField(validators=[validators.DataRequired()])
     last_name = StringField(validators=[validators.DataRequired()])
     email = EmailField(validators=[validators.DataRequired()])
-    password = PasswordField(validators=[validators.DataRequired()])
-    confirm_password = PasswordField(validators=[validators.DataRequired(), validators.EqualTo('password', message='Passwords must match')])
+    password = PasswordField('Senha', validators=[validators.DataRequired(), validators.EqualTo('confirm_password')])
+    confirm_password = PasswordField('Confirmar Senha', validators=[validators.DataRequired(), validators.Length(min=6, max=10)])
     submit = SubmitField()
--- app/controllers/auth/accounts/register.py
+++ app/controllers/auth/accounts/register.py
@@ -1,5 +1,5 @@
 # Flask
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from flask import current_app
 # Flask Extensions
 from flask_login import current_user
@@ -15,13 +15,14 @@ from . import bp
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit():
+
+    if request.method == 'POST' and form.validate():
         first_name = form.first_name.data
         last_name = form.last_name.data
         email = form.email.data
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
 
-        user = User(email=email,password=hashed_password, first_name=first_name, last_name=last_name, active=True)
+        user = User(email=email, password=hashed_password, first_name=first_name, last_name=last_name, active=True)
         default_role = Role.query.filter_by(name='user').first()
 
         if not user.verify_existing(email=email):
@@ -30,14 +31,18 @@ def register():
                 user.roles.append(default_role)
                 db.session.commit()
                 print("Usuário registrado com sucesso.")
+                flash("Usuário registrado com sucesso.", "success")
                 return redirect(url_for('auth.login'))
             except Exception as e:
                 db.session.rollback()
                 print(e)
-                flash("Tente novamente, erro no banco de dados", "error") #? Por que não é exibido?
+                flash("Tente novamente, erro no banco de dados", "error")
                 return render_template('auth/accounts/register.html', form=form)
         else:
-            flash("Tente novamente, erro na autenticação XPTO", "danger")
-            return render_template('auth/accounts/register.html', form=form)
+            flash("Inconsistência no ato do registro, verifique os dados.", "danger")
     else:
-        return render_template('auth/accounts/register.html', form=form)
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash(f'{form[field].label.text}: {error}', 'info')
+
+    return render_template('auth/accounts/register.html', form=form)
