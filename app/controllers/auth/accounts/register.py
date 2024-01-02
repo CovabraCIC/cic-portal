b6493b1de99178c4f1a9c012086e2495735cc821
victorbrax <joaogomespcl@gmail.com>
2024-01-02 09:24:01 -0300

    mensagens flash na autenticação

--- app/controllers/auth/accounts/register.py
+++ app/controllers/auth/accounts/register.py
@@ -1,5 +1,5 @@
 # Flask
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from flask import current_app
 # Flask Extensions
 from flask_login import current_user
@@ -34,5 +34,10 @@ def register():
             except Exception as e:
                 db.session.rollback()
                 print(e)
+                flash("Tente novamente, erro no banco de dados", "error") #? Por que não é exibido?
+                return render_template('auth/accounts/register.html', form=form)
+        else:
+            flash("Tente novamente, erro na autenticação XPTO", "danger")
+            return render_template('auth/accounts/register.html', form=form)
     else:
         return render_template('auth/accounts/register.html', form=form)
