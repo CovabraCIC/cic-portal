50cb82e00798f696f21c070e14e39c896d0294cb
CovabraCIC <cic@covabra.com.br>
2024-01-04 14:16:06 -0300

    fim da sidebar e etapas de login

--- app/controllers/auth/accounts/login.py
+++ app/controllers/auth/accounts/login.py
@@ -1,5 +1,5 @@
 from . import bp
-from flask import render_template, redirect, url_for, flash, current_app
+from flask import render_template, redirect, url_for, flash, session
 from flask_login import login_user, login_required, logout_user, current_user
 from .forms import LoginForm
 from app import login_manager, bcrypt
@@ -22,24 +22,22 @@ def login():
 
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.data["email"]).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        if user and bcrypt.check_password_hash(user.password, form.data["password"]):
             login_user(user)
+            session['logged_in'] = True
             flash("Usuário logado com sucesso.", "success")
-            return redirect(url_for('auth.home'))
+            return redirect(url_for('home.home'))
         else:
-            flash("Inconsistencia no ato do login.", "danger")
+            flash("Inconsistência no ato do login.", "danger")
     return render_template('auth/accounts/login.html', form=form)
 
 @bp.route("/logout")
 @login_required
 def logout():
-    """Encerra a sessão do usuário autenticado, gerando um cookie armazenado no navegador do cliente."""
-    logout_user() 
+    if session.get('logged_in'):
+        session.pop('logged_in', None)
+        logout_user()
+        flash('Usuário deslogado com sucesso.', "success")
+    else:
+        flash("Você não está logado.", "danger")
     return redirect(url_for('auth.login'))
-
-
-@bp.route("/home")
-@login_required
-def home():
-    """Página de exemplo com login requerido para demonstração."""
-    return render_template('home/inicio/index.html', current_user=current_user)
--- app/controllers/home/inicial/routes1.py
+++ app/controllers/home/inicial/routes1.py
@@ -1,8 +1,10 @@
 from flask import render_template
+from flask_login import login_required, current_user
 from . import bp
 
 
-@bp.route('/homepage', methods=['GET', 'POST'])
-def rotaum():
+@bp.route("/", methods=['GET', 'POST'])
+@login_required
+def home():
     # regra de negocio
-    return render_template('home/inicio/index.html')
+    return render_template('home/inicio/index.html', current_user=current_user)
