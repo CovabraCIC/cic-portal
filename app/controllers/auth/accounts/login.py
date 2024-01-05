9bcf5f96667ec1d579e99ad39ea453aee49bde2b
CovabraCIC <cic@covabra.com.br>
2024-01-05 13:55:10 -0300

    alteracoes de base

--- app/controllers/auth/accounts/login.py
+++ app/controllers/auth/accounts/login.py
@@ -1,5 +1,5 @@
 from . import bp
-from flask import render_template, redirect, url_for, flash, session
+from flask import render_template, redirect, url_for, flash, session, g
 from flask_login import login_user, login_required, logout_user, current_user
 from .forms import LoginForm
 from app import login_manager, bcrypt
@@ -14,6 +14,12 @@ login_manager.login_message_category = "warning"
 def load_user(user_id):
     """Define como o Flask-Login carrega um usuário com base no ID armazenado no cookie de sessão.
     Internamente utilizado para carregar o objeto do usuário após autenticação, em todas as solicitações subsequentes."""
+    # if session["user_id"]:
+    #     user = User.query.filter_by(id=session["user_id"]).first()
+    # else:
+    #     user = {"name": "Guest"}  # Make it better, use an anonymous User instead
+    # g.user = user
+
     return User.query.get(int(user_id))
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -21,19 +27,16 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.data["email"]).first()
+        email = form.data["email"]
+        user = User.query.filter_by(email=email).first()
         if user and bcrypt.check_password_hash(user.password, form.data["password"]):
             login_user(user)
             session['logged_in'] = True
+            session['user_id'] = user.id
             flash("Usuário logado com sucesso.", "success")
             return redirect(url_for('home.home'))
         else:
             flash("Inconsistência no ato do login.", "danger")
-    flash("fuck", "danger")
-    flash("fuck", "danger")
-    flash("fuck", "danger")
-    flash("fuck", "danger")
-    flash("fuck", "danger")
     return render_template('auth/accounts/login.html', form=form)
 
 @bp.route("/logout")
--- app/controllers/home/inicial/routes1.py
+++ app/controllers/home/inicial/routes1.py
@@ -1,11 +1,10 @@
 from flask import render_template, flash
-from flask_login import login_required, current_user
+from flask_login import login_required
 from . import bp
 
 
 @bp.route("/", methods=['GET', 'POST'])
 @login_required
 def home():
-    flash("show", "danger")
     # regra de negocio
-    return render_template('home/inicio/index.html', current_user=current_user)
+    return render_template('home/inicio/index.html')
